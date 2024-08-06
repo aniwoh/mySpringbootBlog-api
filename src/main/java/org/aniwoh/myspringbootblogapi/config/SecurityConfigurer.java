2775a339cfdccd32d87f4d4abad19a0709dde61d
aniwoh <aniwoh@126.com>
2024-08-06 11:22:28 +0000

    WebFlux 用MySQL会出问题，准备去学MongoDB了，项目暂停

--- src/main/java/org/aniwoh/myspringbootblogapi/config/SecurityConfigurer.java
+++ src/main/java/org/aniwoh/myspringbootblogapi/config/SecurityConfigurer.java
@@ -4,6 +4,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 import org.springframework.web.server.session.DefaultWebSessionManager;
 import org.springframework.web.server.session.WebSessionManager;
@@ -11,21 +13,24 @@ import org.springframework.web.server.session.WebSessionManager;
 @Configuration
 @EnableWebFluxSecurity
 public class SecurityConfigurer {
-
-
     @Bean
-    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity serverHttpSecurity){
-        return  serverHttpSecurity
+    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity serverHttpSecurity) {
+        return serverHttpSecurity
                 .csrf(ServerHttpSecurity.CsrfSpec::disable) //关闭csrf认证
                 .authorizeExchange((authorize) -> authorize
+                        .pathMatchers("/user/login","/user/register").permitAll()
                         .pathMatchers("/article/upload").hasRole("ADMIN")
                         .pathMatchers("/article/**").permitAll())
-
                 .build();
     }
 
     @Bean
-    public WebSessionManager webSessionManager(){
+    public WebSessionManager webSessionManager() {
         return new DefaultWebSessionManager();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 }
